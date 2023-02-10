d8e11996d77108047211c34484a545fa0ff013ee
ebenezersakyi <ebensakyi0@gmail.com>
2023-02-10 22:57:31 +0000

    commit

--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -37,16 +37,17 @@ const Location = () => {
           <div className='content grid3 mtop'>
             {location.map((item, index) => (
               <div className='box' key={index}>
-                <a onClick={() =>{ if(item.name == 'Your Current Location'){search() } }}></a>
-                <img src={item.cover} alt='' />
-                <div className='overlay'>
-                  <h5>{item.name}</h5>
-                  <p>
-                    <label>{item.Villas}</label>
-                    <label>{item.Offices}</label>
-                    <label>{item.Apartments}</label>
-                  </p>
-                </div>
+                <a onClick={() =>{ if(item.name == 'Your Current Location'){search() } }}>
+                  <img src={item.cover} alt='' />
+                  <div className='overlay'>
+                    <h5>{item.name}</h5>
+                    <p>
+                      <label>{item.Villas}</label>
+                      <label>{item.Offices}</label>
+                      <label>{item.Apartments}</label>
+                    </p>
+                  </div>
+                </a>
               </div>
             ))}
           </div>
